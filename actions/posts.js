dcc9280ada82254422437b3b3821c89157d12e5b
Kushagra Pandey <kushagra.15554@gmail.com>
2024-11-08 23:27:36 +0530

    Final Commit

--- actions/posts.js
+++ actions/posts.js
@@ -39,10 +39,11 @@ export async function createPost(prevState, formData) {
     content,
     userId: 1,
   });
+  revalidatePath("/", "layout");
   redirect("/feed");
 }
 
 export async function togglePostLikeStatus(postId) {
   await updatePostLikeStatus(postId, 2);
-  revalidatePath('/','layout');
+  revalidatePath("/", "layout");
 }
